4894ea9a59560b2e04317eb4b986a57b369c39b9
Nazerke23 <nazerke.sarsenbaevna@gmail.com>
2023-09-29 11:18:51 -0500

    just giving instructions

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,9 @@
 function Error() {
   return (
     <p className="error">
-      <span>💥</span> There was an error fecthing questions.
+      <span>💥</span> There was an error fecthing questions. In order to app to
+      work you need to install npm i json-server, after run command npm run
+      server, and now you should be good.
     </p>
   );
 }
