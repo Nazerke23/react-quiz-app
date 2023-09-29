1cdf3eed1235bdae86178f074776c72567c07e9b
Nazerke23 <nazerke.sarsenbaevna@gmail.com>
2023-09-29 11:58:46 -0500

    test2

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ function Error() {
     <p className="error">
       <span>💥</span> There was an error fecthing questions. In order to app to
       work you need to install npm i json-server, after run command npm run
-      server, and now you should be good.
+      server, and now you should be good. Hello again WORLD!!!
     </p>
   );
 }
